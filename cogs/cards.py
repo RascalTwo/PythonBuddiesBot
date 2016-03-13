978da2d166f6166a8918fa3bd6515906d2f874dd
Rascal_Two <therealrascaltwo@gmail.com>
2016-03-12 21:55:52 -0500

    Redesigned 'HiLo_Game' logic into single  function.. Not ignoring 'line-too-long' warnings. Import order fixed. Made HiLo a subcommand correctly. Added help to 'game' and 'hilo' commands. Only known issue preventing merge with master is missing economy.

--- cogs/cards.py
+++ cogs/cards.py
@@ -3,8 +3,8 @@
 Will contain many card playable card games.
 
 """
-from discord.ext import commands
 import random
+from discord.ext import commands
 
 playing_users = {}
 
@@ -15,14 +15,18 @@ def generate_deck(value_rules, jokers=0, joker_value=0):
     Keyword Arguments:
     jokers -- Number of Jokers to add to Deck. (default 0)
     joker_value -- Value for each Joker generated. (default 0)
-    value_rules -- Value(int)-Rank(str) dictionary for each Card generated. (example {1: "Ace"})
+    value_rules -- Value(int)-Rank(str) dictionary for each Card generated.
+                   (example {1: "Ace"})
 
     Returns:
     list -- List of Cards, each being unique asside from possible Jokers.
+
     """
     suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
-    deck = [Card(value_rules, value, suit) for value in value_rules for suit in suits]
-    deck.extend([Card({joker_value: "Joker"}, joker_value, "") for _ in range(jokers)])
+    deck = [Card(value_rules, value, suit)
+            for value in value_rules for suit in suits]
+    deck.extend([Card({joker_value: "Joker"}, joker_value, "")
+                 for _ in range(jokers)])
     return deck
 
 
@@ -32,7 +36,7 @@ class Card:
     suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
     def __init__(self, value_rules, value, suit):
-        """Create a new Card with the supplied `value` and `suit`, getting rank name from `value_rules`.
+        """Create a new Card with the supplied `value` and `suit`.
 
         Keyword Arguments:
         value_rules -- Value(int)-Rank(str) dictionary (example {1: "Ace"})
@@ -54,7 +58,10 @@ class Card:
         bool -- True if this Card and 'other' Card are equal, False otherwise.
 
         """
-        return self.__class__ == other.__class__ and self.rank == other.rank and self.suit == other.suit and self.value == other.value
+        return (self.__class__ == other.__class__ and
+                self.rank == other.rank and
+                self.suit == other.suit and
+                self.value == other.value)
 
     @property
     def rank(self):
@@ -76,13 +83,15 @@ class Card:
         """Return random Card of one of the `value_rules`.
 
         Keyword Arguments:
-        value_rules -- Value(int)-Rank(str) dictionary for each Card generated. (example {1: "Ace"})
+        value_rules -- Value(int)-Rank(str) dictionary for each Card generated.
+                       (example {1: "Ace"})
 
         Returns:
         card - Randomly chosen card from `value_rules`.
 
         """
-        return Card(value_rules, random.choice(list(value_rules.keys())), random.choice(["Clubs", "Diamonds", "Hearts", "Spades"]))
+        return Card(value_rules, random.choice(list(value_rules.keys())),
+                    random.choice(["Clubs", "Diamonds", "Hearts", "Spades"]))
 
 
 class HiLo_Game:
@@ -103,7 +112,9 @@ class HiLo_Game:
         12: "Queen",
         13: "King"
     }
-    responces = {"guess": ["higher", "lower"], "choose": ["double", "pass"]}
+    responces = {"start": [None],
+                 "guess": ["higher", "lower"],
+                 "choose": ["double", "pass"]}
 
     def __init__(self, user, bet_amount):
         """Create a new HiLo game with the supplied player.
@@ -115,7 +126,7 @@ class HiLo_Game:
         """
         self.user = user
         self.bet_amount = int(bet_amount)
-        self.stage = "guess"
+        self.stage = "start"
         self.card_hidden = None
         self.card_visible = None
         self._new_cards()
@@ -126,65 +137,74 @@ class HiLo_Game:
         while self.card_hidden == self.card_visible:
             self.card_hidden = Card.random(self.value_rules)
 
-    def get_message(self, message, params=None):
-        """Return a Message.
+    def logic(self, message=None):
+        """Contain all Game execution logic.
 
         Keyword Arguments:
-        message -- Message to get.
+        message -- Message from the user. (default None)
 
         Returns:
-        str -- Message with variables filled in.
+        str or None -- Returns a message to say into the chat, if any.
 
         """
-        if message == "game_start":
-            return "So, do you think the face-down card is *higher* or *lower* then the **{}**?".format(self.card_visible)
-        elif message == "double_or_pass":
-            return "Do you wish to go for *double* or nothing, or will you take your winnings and *pass*?"
-        elif message == "user_won":
-            return "You Won $**{}**!\nDo you wish to go for *double* or nothing, or will you take your winnings and *pass*?".format(self.bet_amount * 2)
-        elif message == "user_lose":
-            return "You Lost $**{}**!\nBetter luck next time!".format(self.bet_amount)
-        elif message == "guess_result":
-            return "The **{}** {}{} then the **{}**".format(self.card_hidden, "is " if params[0] else "is not ", params[1], self.card_visible)
-        elif message == "game_won":
-            return "Thanks for playing!\n$**{}** has been added to your account!".format(self.bet_amount * 2)
-
-    def guess(self, guess):
-        """Guess weather the hidden card is higher or lower than the hidden card.
-
-        Returns:
-        bool -- True if the user was correct, False otherwise.
-
-        """
-        if guess.lower() == "higher"and self.card_hidden.value > self.card_visible.value:
-            self.stage = "choose"
-            return True
-        elif guess.lower() == "lower" and self.card_hidden.value < self.card_visible.value:
-            self.stage = "choose"
-            return True
+        if message is not None:
+            message.content = message.content.lower()
+            if message.content not in self.responces[self.stage]:
+                return
         else:
-            return False
+            if message not in self.responces[self.stage]:
+                return
 
-    def choose(self, choice):
-        """Choose to either go for double or nothing, or to pass and take your winnings.
-
-        Keyword Arguments:
-        choice -- User choice, either being `double` or `pass`.
-
-        Returns:
-        bool -- True if the game is continuing, False otherwise
-
-        """
-        if choice.lower() == "double":
-            if False:  # User doesn't have enough money to go double or nothing
-                return False
-            self.bet_amount *= 2
-            self._new_cards()
+        if self.stage == "start":
             self.stage = "guess"
-            return True
-        elif choice.lower() == "pass":
-            self.stage = "won"
-            return False
+            return ("So, do you think the face-down card is `higher` or "
+                    "`lower` then the **{}**?".format(self.card_visible))
+
+        if self.stage == "guess":
+            responce = ""
+            if ((message.content == "higher" and
+                 self.card_hidden.value > self.card_visible.value) or
+                    (message.content == "lower" and
+                     self.card_hidden.value < self.card_visible.value)):
+                responce += ("You Won $**{}**!\nDo you wish to go for "
+                             "`double` or nothing, or will you take your"
+                             "winnings and `pass`?"
+                             .format(self.bet_amount * 2))
+                is_or_is_not = "is"
+                self.stage = "choose"
+            else:
+                responce += ("You Lost $**{}**!\nBetter luck next time!"
+                             .format(self.bet_amount))
+                is_or_is_not = "is not"
+#               TODO: Remove self.bet_amount from user account.
+                del playing_users[message.author.id]
+            responce = ("The **{}** {} {} then the **{}**\n"
+                        .format(self.card_hidden,
+                                is_or_is_not,
+                                message.content,
+                                self.card_visible)) + responce
+            return responce
+
+        elif self.stage == "choose":
+            if message.content == "double":
+                if True:
+                    self._new_cards()
+                    responce = ("So, do you think the face-down card is "
+                                "`higher` or `lower` then the **{}**?"
+                                .format(self.card_visible))
+#                   TODO: Replace with economy check.
+                    self.bet_amount *= 2
+                    self.stage = "guess"
+                else:
+                    responce = ("You don't have ${} to bet, passing instead\n"
+                                .format(self.bet_amount))
+                    message.content = "pass"
+            if message.content == "pass":
+                responce = ("Thanks for playing!\n$**{}** has been added to "
+                            "your account!".format(self.bet_amount * 2))
+#               TODO: Add self.bet_amount * 2 to user account.
+                del playing_users[message.author.id]
+            return responce
 
 
 class Cards:
@@ -194,58 +214,65 @@ class Cards:
         """Initalization function."""
         self.bot = bot
 
-    @commands.command(pass_context=True)
+    @commands.group(pass_context=True)
     async def game(self, ctx):
         """Base command for playing card games.
 
-        Keyword arguments:
-        ctx -- Command context.
+        Playable games:
+
+        HiLo - game hilo bet_amount
 
         """
-        arguments = ctx.message.content.split(" ")
-        game_name = arguments[1]
-        if game_name.lower() == "hilo":
-            if len(arguments) != 3:
-                await self.bot.say("Correct command: 'game hilo bet_amount'\nExample: 'game hilo 10'")
-                return
-            if not arguments[2].isdecimal():
-                await self.bot.say("Bet amount needs to be a number.")
-                return
-            if False:  # TODO: Replace with not enough money economy check.
-                await self.bot.say("You don't have ${}!".format(arguments[2]))
-                return
-            hilo_game_instance = HiLo_Game(ctx.message.author, arguments[2])
-            await self.bot.say(hilo_game_instance.get_message("game_start"))
-            playing_users[ctx.message.author.id] = hilo_game_instance
-#                   FIX: Above error messages are only placeholders at the moment.
+        if ctx.invoked_subcommand is None:
+            await self.bot.say("Try one of these games:\n"
+                               "HiLo - game hilo bet_amount")
+
+    @game.command(pass_context=True)
+    async def hilo(self, ctx):
+        """Start a game of HiLo.
+
+        HiLo is a game in which two unique cards are randomly generated. You
+        are then told the value of one of these cards, and must guess if the
+        other value of the other card is either higher, or lower then the value
+        of the first card.
+
+        If you are correct, you win and may either take your money, or go for
+        double or nothing!
+
+        Values from least to greatest:
+            Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King
+
+        Command: game hilo bet_amount
+        Example: game hilo 10
+
+        """
+        arguments = ctx.message.content.lower().split(" ")
+        if len(arguments) != 3:
+            await self.bot.say("```Command: game hilo bet_amount\n"
+                               "Example: game hilo 10```")
+            return
+        if not arguments[2].isdecimal():
+            await self.bot.say("```Bet amount needs to be a number.```")
+            return
+        if False:
+            await self.bot.say("```You don't have $**{}**!```"
+                               .format(arguments[2]))
+#           TODO: Replace with economy check.
+            return
+        hilo_game_instance = HiLo_Game(ctx.message.author, arguments[2])
+        await self.bot.say(hilo_game_instance.logic())
+        playing_users[ctx.message.author.id] = hilo_game_instance
 
     async def receive_message(self, message):
         """Called whenever any player sends a message."""
-        if message.author.id == self.bot.user.id or message.author.id not in playing_users:
+        if (message.author.id == self.bot.user.id or
+                message.author.id not in playing_users):
             return
         game = playing_users[message.author.id]
         if isinstance(game, HiLo_Game):
-            if message.content.lower() not in game.responces[game.stage]:
-                return
-            if game.stage == "guess":
-                won = game.guess(message.content.lower())
-                responce_message = game.get_message("guess_result", [won, message.content.lower()])
-                if won:
-                    responce_message += "\n" + game.get_message("user_won")
-                else:
-                    responce_message += "\n" + game.get_message("user_lose")
-#                   TODO: Remove game.bet_amount from user account.
-                    del playing_users[message.author.id]
-                await self.bot.send_message(message.channel, responce_message)
-            elif game.stage == "choose":
-                double = game.choose(message.content.lower())
-                if double:
-                    responce_message = game.get_message("game_start")
-                else:
-                    responce_message = game.get_message("game_won")
-#                   TODO: Add game.bet_amount * 2 to user account.
-                    del playing_users[message.author.id]
-                await self.bot.send_message(message.channel, responce_message)
+            responce = game.logic(message)
+            if responce is not None:
+                await self.bot.send_message(message.channel, responce)
 
 
 def setup(bot):
